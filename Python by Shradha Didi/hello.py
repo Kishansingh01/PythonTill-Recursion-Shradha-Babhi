f07aecbd2606c5943c597e94d4dfc1658adcdb7d
Kishansingh01 <kishansingh9006866@gmial.com>
2024-06-02 23:49:24 +0530

    Basic till recursio shradha babhi

---
+++ Python by Shradha Didi/hello.py
@@ -0,0 +1,115 @@
+#print("i am kishan ")
+#name="Kishan"
+#print("name")
+#print(name)
+#name="kishan singh rajput"
+#age=19
+#is_adult=True
+#print(name)
+#print(age)
+#first_name=" tony"
+#last_name="starc"
+#age=43
+#is_genius=True
+#print(first_name)
+#print(last_name)
+#print(age)
+#old_age=input("enter your old age  ")
+#new_age=int(old_age)+2
+#print(new_age)
+#number=18
+#print(float(18))
+#first=input("Enter the first number:")
+#second=input("Enter the second number:")
+#sum=int(first)+int(second)
+#print(sum)
+#name="kishan singh rajput"
+#print(name.upper())
+#print(name.find('s'))
+#print(name.replace("kishan singh","Khushboo Singh"))
+#name="kishan singh rajput"
+#print(name.replace("kishan singh",Khushboo Singh))
+#name="Tony Starc"
+#print(name.replace("T","M"))
+#name="ram is god for me"
+#print("ram"in name)
+#print(5+5)
+#print(6-2)
+#print(5*7)
+#print(5/2)
+#print(5//2)
+#print(5%2)
+#print(5**2)
+#print(6**2)
+#i=5
+#print(i+=2)
+#result= 2+4/5
+#print(result)
+#print(2+5//5)
+# print(3>2)
+# print(3<2)
+# print(3<=3)
+# print(3>=3)
+# print(3>=2)
+# print(3<=2)
+# print(3!=2)
+# print(2!=3)
+# print(3>2 or 2>3)
+# print(2>3 and 3>2)
+# print(not 2>3)
+# print(not 3>2)
+# age=19
+# if(age>=19):
+# print("You are an adult")
+# print("You can vote")
+# print("thank you")
+
+# age=18
+# if age>=18:
+#     print("You are an adult")
+#     print("you can vote")
+
+#     print("thank you")
+
+# #     first=input("Enter first number")
+# #     operator=input("Enter operator(+,-,*,/,%)")
+# #     second=input("Enter second number")
+# #     first=int(first)
+# #     second=int(second)
+# #     if operator=="+":
+# #         print(first+second)
+# # elif operator=="-":
+# #     print(first-second)
+# # elif operator=="*":
+# #     print(first*second)
+# # elif operator=="/":
+# #     print(first/second)
+# # elif operator=="%":
+# #     print(first%second)
+# # else:
+# #     print(Invalid operation)  
+number=range(0,19)   
+print(number)         
+i=1
+while (i<=5):
+    print(i)
+    i=i+1
+
+    a=10
+    while(i<=10000):
+    print(i)
+    i=i+5
+
+    i=5
+while(i>=0):
+    print(i *"*")
+    i=i-1
+
+marks=[89,87,99,100,94]
+for score in marks:
+    print(score)
+
+
+
+
+
---
+++ loops.py
@@ -0,0 +1,315 @@
+
+# count=4 
+# while count<=5:
+#     print('Jai maa saraswati')
+#     count+=1
+#     print(count)
+# i=1
+# while i<=4:
+#     print("i am kishan")
+#     i+=1
+#     print(count)
+# ram=100
+# while ram<=120:
+#     print("i am kishan and i am in a stress",ram)
+#     ram+=5
+# i=1
+# while(i<=5):
+#     print(i)
+#     i+=1
+# i=10
+# while(i>=1):
+#     print(i)
+#     i-=1
+# ok=101
+# while(ok>=1):
+#     ok-=1
+#     print(ok)
+
+# i=1
+# while(i<=10):
+#     print(3*i)
+#     i+=1
+# kishan=1
+# while(kishan<=10):
+#     print(19*kishan)
+#     kishan+=1
+
+# poonam=1
+# while(poonam<=11):
+#     print(20*poonam)
+#     poonam=poonam+1
+
+# n=int(input("Enter a number;"))
+# i=1
+# while(i<=10):
+#     print(n*i)
+#     i=i+1
+
+# i=int(input("Enter any number"))
+# print(i*i)
+
+# i=[12,34,5,6,57,67,878,78,78,6,4,4,33,443,454]
+# index=0
+# while index<len(i):
+#     print(i[index])
+#     index+=1
+#SEARCCH FOR THE LIST
+# heroes=["ironmam","Thor","superman","batman","spiderman","superwoman"]
+# i=0
+# while i<len(heroes[i]):
+#     print(heroes[i])
+#     i+=1
+
+
+# SEARCH FOR THE TUPLE
+# tuple=(23,54,67,78,34,567,785,365)
+# i=0
+# while i<len(tuple):
+#     print (tuple[i])
+#     i+=1
+
+#FINDING POSITION OF THE INDEX OF AN ELEMENTS
+
+# nums=(1,34,5675,35,786,87,23,67)
+# x=34
+# i=0
+# while i<len(nums):
+#     if(nums[i]==x):
+#         print("found at index",i)
+
+# i=1
+# while i<=5:
+#    print(i)
+#    if(i==3):
+#       break
+#    i=i+1
+# i=1
+# while(i<10):
+#     if(i==3):
+#         i+=1
+#         continue
+#     print(i)
+#     i=i+1
+
+
+# i=1   for odd number
+# while(i<11):
+#     if(i%2==0):
+#         i=i+1
+#         continue
+#     print(i)
+#     i=i+1
+
+# i=1
+# while(i<101):
+#     if(i%2!=0):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+# list=["Ram","sita","Hanuman","lord"]
+# for i in list:
+#     print(i)
+
+
+# tuple=("ramahan","abdul kalam","Adani ji")
+# for kishan in tuple:
+#     print(kishan)
+# Name="kishankumar"
+# for column in Name:
+#     print(column) 
+
+# Function and Recurresion 
+# def value_count(a,b):
+#     sum=a+b 
+#     print(sum)
+#     return sum
+# value_count(5,9)
+# value_count(8,3)
+# value_count(28971987,98089)
+
+# def Ram(a,o):
+#     subtract=a-o
+#     print(subtract)
+#     return subtract
+# Ram(9897,234)
+# Ram(323,453)
+# Ram(5453,356354)
+
+# def shyam(a,b):
+#     return a+b
+# Ram=shyam(86,989)
+# print(Ram)
+# Sita=shyam(8786,85)
+# print(Sita)
+# Madhubala=shyam(34,543)
+# print(Madhubala)
+
+# def Ram():
+#     print("ram ram sita ram")
+# Ram()
+
+# def print_hello():
+#     print("hello")
+# print_hello()
+
+# def kishan():
+#     print("i am kishan singh rajput")
+#     print('i am king')
+#     print(" time management")
+# kishan()    
+
+# def i_am():
+#     print("i am is the champion")
+# Output= i_am( )
+# print(output)
+   
+# def avg(a,b,c):
+#     average=(a+b+c)/3
+#     print(average)
+#     return average
+# avg(3,6,9)
+# avg(3,3,3)
+
+# def avg(a,b,c):
+#     return (a+b+c)/3
+# Ram=avg(3,6,9)
+# print(Ram)
+
+
+# def average(a,b,c):
+#     avg=(a+b+c)/3
+#     print(avg)
+#     return avg
+# average(4,8,9)
+    
+# print(" i am is the kishan singh",end="            ")
+# print("kishan singh rajput")
+    
+# cities=["london","Newyork","New Delhi","Bejiging","Tokyo","Berlin","madrid"]
+# heroes=["Leanardo de Gabrio","Taylor Swift","Tim kook","Nick jonas","Tom latham"]
+# My_family_member=["i am","my father","my mumma","my sisters"]
+# def print_len(list):
+#     print(len(list))
+# print_len(cities)    
+# print_len(heroes)
+# print_len(My_family_member)
+# cities=["mumbai","Pune","Noida","New Delhi"]
+# heroes=["ironman","Ram","netaji subhash chandra bose","MahatmaGandhi"]
+# def print_len(list):
+#     print(len(list))
+# def print_list(list):
+#         for item in list:
+#             print(item,end=" ")
+# print_list(cities)
+# print()
+# print_list(heroes)
+# print()
+
+# By loop method(factorial)
+
+# fact=1
+# i=5
+# for i in range(1,i+1):
+#     fact*=i
+# print(fact)
+
+# def fact(n):
+#     fact=1
+#     for i in range(1,n+1):
+#         fact*=i
+#     print(fact)
+        
+# fact(10)
+# fact(4)
+
+# def fact(n):
+#     fact=1
+#     for i in range(1,n+1):
+#         fact*=i
+#     print(fact)
+#     return fact
+# fact(5)
+# fact(6)
+# fact(3)
+
+#convert USD into INRdef
+# converter(usd_value):
+#     inr_value=usd_value*83
+#     print(usd_value,"USD=",inr_value,"INR")
+
+# converter(5)
+
+# def number(n):
+#     if n%2!=0:
+#         print("odd")
+#     else:
+#         print("even")
+#         return number
+# number(5) 
+# number(10)  
+
+#RECURSION
+# def show(n):
+#     if(n==0):
+#         return
+#     print(n)
+#     show(n-1)
+#     print("END")
+# show(5)    
+
+
+# show(5)    
+
+# def Ram(k):
+#     if(k==101):
+#         return
+#     print(k)
+#     Ram(k+1)
+# Ram(1)  
+
+
+# i=1
+# while(i<11):
+#     print(4*i)
+#     i+=1
+
+# n=int(input("Enter a number   ;"))
+# i=1
+# while(i<=10):
+#     print(n*i)
+#     i+=1
+
+# def table(n):  
+#     if (n==)
+
+#write factorial for the given in recurrsion form
+
+# def factorial(n):
+#     if(n==0 or n==1):
+#         return 1
+#     return factorial (n-1)*n
+
+# print(factorial(5))
+# print(factorial(4))
+
+# def cal_sum(n):
+#     if(n==0):
+#         return 0
+#     return cal_sum(n-1)+n
+# sum=cal_sum(5)
+# print(sum)
+
+def print_list(list,idx=0):
+    if(idx==len(list)):
+        return
+    print(list(idx))
+    print_list(list,idx+1)
+    fruits=["apple","bananas","oranges","grapes"]
+    print_list(fruits)
+
+
+
+    
